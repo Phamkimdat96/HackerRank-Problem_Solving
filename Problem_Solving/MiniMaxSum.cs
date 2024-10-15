7cd0bde333138d856405c1ff51dc951b96be4425
pkdat96 <phamkimdat@upmesh.io>
2024-10-15 23:04:50 +0700

    Add project files.

---
+++ Problem_Solving/MiniMaxSum.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        long minSum = 0;
+        long maxSum = 0;
+        var length = arr.Count();
+        var n = length - 1;
+        for (int i = 0; i < length - 1; i++)
+        {
+            minSum = minSum + arr[i];
+            maxSum = maxSum + arr[n];
+            n--;
+        }
+        Console.Write($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
